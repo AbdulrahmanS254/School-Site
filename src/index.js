174a783fe6ce68cb5bc08c21549d46d757aab532
Abdulrahman Shawky <hawk18922@gmail.com>
2024-09-19 01:13:33 +0300

    navbar not finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from "bootstrap";
+import "./assets/sass/style.scss";
 
-import './assets/sass/style.scss';
+const navbar = document.getElementById("navbar");
+
+// Add an event listener for the scroll event
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        // Adjust scrollY value to when you want the color change
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
