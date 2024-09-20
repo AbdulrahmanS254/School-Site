6e477b1907d5442dc08d3ead06498f59ec59fa32
Abdulrahman Shawky <hawk18922@gmail.com>
2024-09-20 21:42:48 +0300

    students section is not done yet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import * as bootstrap from "bootstrap";
 import "@fortawesome/fontawesome-free/js/all.js";
+import Swiper from "swiper";
+import "swiper/css/bundle";
 import "./assets/sass/style.scss";
 
 // Changing the navbar background when scrolling
@@ -13,11 +15,49 @@ window.addEventListener("scroll", () => {
 });
 
 // changing the background when the button is clicked
-const navButton = document.getElementById("nav-btn")
+const navButton = document.getElementById("nav-btn");
 navButton.addEventListener("click", () => {
-    if (navButton.classList.contains('collapsed')) {
-        navbar.classList.remove('bg-change');
+    if (navButton.classList.contains("collapsed")) {
+        navbar.classList.remove("bg-change");
     } else {
-        navbar.classList.add('bg-change');
+        navbar.classList.add("bg-change");
     }
+});
+
+// Students swiper
+// const swiper = new Swiper(".swiper", {
+//     // Optional parameters
+//     direction: "horizontal",
+//     loop: true,
+
+//     // If we need pagination
+//     pagination: {
+//         el: ".swiper-pagination",
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+
+const gallerySlider = new Swiper(".swiper.is-gallery", {
+    loop: true,
+    slidesPerView: 2,
+    centeredSlides: true,
+    speed: 300,
+    grabCursor: true,
+    parallax: true,
+});
+
+// Dark and Light Style
+const control = document.getElementById("direction-toggle");
+const wrapper = document.querySelector(".slideStyle");
+const footer = document.querySelector(".footer");
+
+control.addEventListener("click", () => {
+    control.classList.toggle("darkStyle");
+    wrapper.classList.toggle("darkStyle");
+    footer.classList.toggle("darkStyle");
 });
